42ab3d672318ca8ca4d3af9206987f47e3749201
Prahlad MV <79750338+PrahladMV@users.noreply.github.com>
2024-01-01 15:44:52 +0530

    Update doublelinkedlist_hashmap.cpp
    Design a data structure for LRU Cache. It should support the following operations: get and set.
    
    get(key) – Get the value (will always be positive) of the key if the key exists in the cache, otherwise
    return -1.
    set(key, value) – Set or insert the value if the key is not already present.

--- doublelinkedlist_hashmap.cpp
+++ doublelinkedlist_hashmap.cpp
@@ -1,3 +1,7 @@
+/* Design a data structure for LRU Cache. It should support the following operations: get and set.
+get(key) – Get the value (will always be positive) of the key if the key exists in the cache, otherwise
+return -1. set(key, value) – Set or insert the value if the key is not already present. */
+
 #include <iostream>
 #include <unordered_map>
 
